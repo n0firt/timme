36b303cf4cf049e1047e0479c1b823b164746cd0
n0firt <no.sense.found@tuta.io>
2026-02-26 14:04:26 +0300

    project init

---
+++ frontend/vite.config.js
@@ -0,0 +1,30 @@
+/* global process */
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      host: true,
+      port: 5173,
+      watch: {
+        usePolling: true,
+      },
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
+})
